705d1112538aba48aaf04e7dbf05f347c81e27ec
Karl Hornlund <karlhornlund@gmail.com>
2019-07-09 19:57:35 +0930

    Removed unused imports

--- aptos/model/model.py
+++ aptos/model/model.py
@@ -1,9 +1,7 @@
-import math
 from collections import OrderedDict
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision.models as models
 from efficientnet_pytorch import EfficientNet
 
